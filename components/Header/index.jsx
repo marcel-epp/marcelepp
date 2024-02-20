060b44e42d84b447bf6d7721a88917cdeae39876
Marcel Epp <epp.marcel@icloud.com>
2024-02-20 19:14:31 +0100

    add header and styled components

---
+++ components/Header/index.jsx
@@ -0,0 +1,11 @@
+import { StyledHeader } from "./Header.Styled";
+
+export default function Header() {
+  return (
+    <>
+      <StyledHeader>
+        <h1>Hello from NextJS!</h1>
+      </StyledHeader>
+    </>
+  );
+}
